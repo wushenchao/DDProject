1111f6ff24f2effd7ae56cfeb3b8bedb68a66c49
吴申超 <625574612@qq.com>
2019-11-05 13:58:08 +0800

    496. 下一个更大元素 I

--- DDLC/DDLC/DDStack.swift
+++ DDLC/DDLC/DDStack.swift
@@ -79,4 +79,50 @@ class DDStack: NSObject {
             return 0
         }
     }
+    
+    // 496. 下一个更大元素 I
+    func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        /*
+         给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+         nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+         */
+        var dic: [Int: Int] = [:]
+        var res: [Int] = []
+        for num in nums2 {
+            if nums1.contains(num) {
+                dic[num] = -1
+            }
+            for k in dic.keys {
+                if dic[k]! == -1 && k < num {
+                    dic[k] = num
+                }
+            }
+        }
+        for num in nums1 {
+            let v = dic[num]
+            if v == nil {
+                res.append(-1)
+            }
+            else {
+                res.append(v!)
+            }
+        }
+        return res
+    }
+    
+    func nextGreaterElement1(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        /*
+         给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+         nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+         */
+        var stack: [Int] = []
+        var dict: [Int:Int] = [:]
+        for num in nums2 {
+            while !stack.isEmpty && stack.last! < num {
+                dict[stack.popLast()!] = num
+            }
+            stack.append(num)
+        }
+        return nums1.map{dict[$0] ?? -1}
+    }
 }
