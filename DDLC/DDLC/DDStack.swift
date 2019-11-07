aa8207641e1f5a11f282860dc0a3a20b106fdb85
吴申超 <625574612@qq.com>
2019-11-07 09:36:04 +0800

    331. 验证二叉树的前序序列化

--- DDLC/DDLC/DDStack.swift
+++ DDLC/DDLC/DDStack.swift
@@ -467,4 +467,43 @@ extension DDStack {
         }
         return stack.popLast()!
     }
+    
+    // 331. 验证二叉树的前序序列化
+    func isValidSerialization(_ preorder: String) -> Bool {
+        /**
+         序列化二叉树的一种方法是使用前序遍历。当我们遇到一个非空节点时，我们可以记录下这个节点的值。如果它是一个空节点，我们可以使用一个标记值记录，例如 #。
+         例如，上面的二叉树可以被序列化为字符串 "9,3,4,#,#,1,#,#,2,#,6,#,#"，其中 # 代表一个空节点。
+         给定一串以逗号分隔的序列，验证它是否是正确的二叉树的前序序列化。编写一个在不重构树的条件下的可行算法。
+         每个以逗号分隔的字符或为一个整数或为一个表示 null 指针的 '#' 。
+         你可以认为输入格式总是有效的，例如它永远不会包含两个连续的逗号，比如 "1,,3" 。
+         */
+        if preorder == "#" { return true }
+        let preorders = preorder.split(separator: ",")
+        var stack: [String] = []
+        var count = 0
+        var pre_count = preorders.count
+        for pre in preorders {
+            pre_count -= 1
+            stack.append(String(pre))
+            if pre == "#" {
+                count += 1
+            } else {
+                count = 0
+            }
+            while count == 2 {
+                if stack.count == 2 {
+                    return false
+                } else if stack.count == 3 {
+                    return pre_count == 0
+                } else {
+                    for _ in 0..<3 {
+                        let _ = stack.popLast()
+                    }
+                    count = stack.last! == "#" ? 2 : 1
+                    stack.append("#")
+                }
+            }
+        }
+        return stack.count == 0
+    }
 }
