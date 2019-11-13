0c5a47b334154f04df08201d8160449bbe649a81
吴申超 <625574612@qq.com>
2019-11-13 09:21:39 +0800

    455. 分发饼干

--- DDLC/DDLC/DDGreedy.swift
+++ DDLC/DDLC/DDGreedy.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class DDGreedy: NSObject {
 
+    func swap(_ items: inout [Int], _ lc: Int, _ rc: Int) {
+        let lcV = items[lc]
+        items[lc] = items[rc]
+        items[rc] = lcV
+    }
+    
+    func quickSort(_ datas: inout [Int]) {
+        
+        func partition(_ datas: inout [Int], _ start: Int, _ end: Int) -> Int {
+            let pivort = datas[start]
+            var j = start
+            for i in start+1..<end {
+                if datas[i] < pivort {
+                    j += 1
+                    swap(&datas, i, j)
+                }
+            }
+            swap(&datas, start, j)
+            return j
+        }
+        
+        func quickSortT(_ datas: inout [Int], _ start: Int, _ end: Int) {
+            if start < end {
+                swap(&datas, start, end-1)
+                let m = partition(&datas, start, end)
+                quickSortT(&datas, start, m)
+                quickSortT(&datas, m+1, end)
+            }
+        }
+        quickSortT(&datas, 0, datas.count)
+    }
+    
     /// 122. 买卖股票的最佳时机 II
     func maxProfit(_ prices: [Int]) -> Int {
         /**
@@ -71,5 +103,31 @@ class DDGreedy: NSObject {
             }
         }
         return index == ss.count
+    }
+    
+    /// 455. 分发饼干
+    func findContentChildren(_ g: [Int], _ s: [Int]) -> Int {
+        /**
+         假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j ，都有一个尺寸 sj 。如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+         注意：你可以假设胃口值为正。 一个小朋友最多只能拥有一块饼干。
+         输入: [1,2,3], [1,1]
+         输出: 1 解释: 你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。
+         虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。
+         所以你应该输出1。
+         */
+        if g.count == 0 || s.count == 0 {
+            return 0
+        }
+        var gs = g
+        var ss = s
+        quickSort(&gs)
+        quickSort(&ss)
+        var gi = 0
+        for ssv in ss {
+            if gi < gs.count && ssv >= gs[gi] {
+                gi += 1
+            }
+        }
+        return gi
     }
 }
