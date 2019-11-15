ee5119e85cd0c377a323d0bca4abfb66dbba7822
吴申超 <625574612@qq.com>
2019-11-15 18:42:34 +0800

    922. 按奇偶排序数组 II

--- DDLC/DDLC/DDSort.swift
+++ DDLC/DDLC/DDSort.swift
@@ -35,4 +35,33 @@ class DDSort: NSObject {
         }
         return res
     }
+    
+    /// 922. 按奇偶排序数组 II
+    func sortArrayByParityII(_ A: [Int]) -> [Int] {
+        /**
+         给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+         对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+         你可以返回任何满足上述条件的数组作为答案。
+         示例：
+         输入：[4,2,5,7]
+         输出：[4,5,2,7]
+         解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+         */
+        var res = A
+        var end = A.count - 1
+        for i in stride(from: 0, to: A.count, by: 2) {
+            let v = res[i]
+            if v % 2 != 0 {
+                for j in stride(from: end, to: 0, by: -2) {
+                    if res[j] % 2 == 0 {
+                        end = j
+                        res[i] = res[j]
+                        res[j] = v
+                        break
+                    }
+                }
+            }
+        }
+        return res
+    }
 }
